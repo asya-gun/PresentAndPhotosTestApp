b606d7896df624b25cc2263e10f86c2390337414
Asya Checkanar <asyach3ck@gmail.com>
2024-10-31 16:21:11 +0300

    Initial Commit

---
+++ PresentAndPhotosTestAppTests/PresentAndPhotosTestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentAndPhotosTestAppTests.swift
+//  PresentAndPhotosTestAppTests
+//
+//  Created by Asya Checkanar on 31.10.2024.
+//
+
+import Testing
+@testable import PresentAndPhotosTestApp
+
+struct PresentAndPhotosTestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

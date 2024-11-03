9568ff5946a1ed07e412a0574bf27e6f2da48997
Asya Checkanar <asyach3ck@gmail.com>
2024-11-03 23:47:04 +0300

    Task 2: add Banner with animation

--- PresentAndPhotosTestApp/ViewController.swift
+++ PresentAndPhotosTestApp/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
         label.text = "Present UI Element"
         return label
     }()
+    
+    private let bannerView: BannerView = {
+        let view = BannerView(frame: CGRect(x: 0, y: 0, width: 343, height: 108))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        return view
+    }()
     private let presentButton: PresentButton = {
         let button = PresentButton(frame: CGRect(x: 0, y: 0, width: 168, height: 168))
         button.layer.cornerRadius = 168/2
@@ -27,9 +34,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        view.addSubview(bannerView)
         view.addSubview(presentButton)
         view.addSubview(headerLabel)
         presentButton.startTimer()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            self.bannerView.animateBanner()
+        })
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -37,6 +48,10 @@ class ViewController: UIViewController {
     }
     func setConstraints() {
         NSLayoutConstraint.activate([
+            bannerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bannerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            bannerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            bannerView.heightAnchor.constraint(equalToConstant: 108),
             headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             headerLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -150),
             presentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
--- PresentAndPhotosTestApp/Views/BannerView.swift
+++ PresentAndPhotosTestApp/Views/BannerView.swift
@@ -8,13 +8,91 @@
 import UIKit
 
 class BannerView: UIView {
+    private var banner: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(resource: .bannerBackground)
+        view.layer.cornerRadius = 12
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private var collageImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(resource: .collage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    private var headerLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        label.text = "Try three days free trial"
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private var textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white.withAlphaComponent(0.5)
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.text = "You will get all premium templates, additional stickers and no ads"
+        label.textAlignment = .left
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
     }
-    */
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupView()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        banner.frame = bounds
+        setupConstraints()
+        banner.frame.origin.y = -banner.frame.size.height
+    }
+    private func setupView() {
+        self.addSubview(banner)
+        banner.addSubview(collageImageView)
+        stackView = UIStackView(arrangedSubviews: [headerLabel, textLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .leading
+        banner.addSubview(stackView)
+        
+    }
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            banner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            banner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            banner.widthAnchor.constraint(equalToConstant: bounds.width),
+            banner.heightAnchor.constraint(equalToConstant: bounds.height),
+            
+            collageImageView.topAnchor.constraint(equalTo: banner.topAnchor, constant: 14),
+            collageImageView.bottomAnchor.constraint(equalTo: banner.bottomAnchor, constant: -10),
+            collageImageView.trailingAnchor.constraint(equalTo: banner.trailingAnchor, constant: -20),
+            stackView.topAnchor.constraint(equalTo: banner.topAnchor, constant: 22),
+            stackView.bottomAnchor.constraint(equalTo: banner.bottomAnchor, constant: -22),
+            stackView.leadingAnchor.constraint(equalTo: banner.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: collageImageView.leadingAnchor, constant: -20)
+            ])
+    }
+    func animateBanner() {
+        UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            self.banner.frame.origin.y = 0
+        })
+    }
 }
